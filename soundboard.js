717b2a3778b0108cc3445493cef2f09b0d3765a2
19barsav <barne329@msu.edu>
2022-06-21 20:22:56 -0400

    now?

--- soundboard.js
+++ soundboard.js
@@ -2,29 +2,29 @@ var buttonElement1 = document.getElementById('carrie')
 var buttonElement2 = document.getElementById('harrison')
 var buttonElement3 = document.getElementById('blaster')
 
+buttonElement1.addEventListener('click', sound_play("carrie"))
+buttonElement2.addEventListener('click', sound_play("harrison"))
+buttonElement3.addEventListener('click', sound_play("blaster"))
 
-
+const music1 = new Audio('dont_like.wav');
+const music2 = new Audio('bad_feeling.wav');
+const music3 = new Audio('blaster.wav');
+        
+        
 
 var sound_play = function (sound) {
     if (sound == 'carrie') {
-        const music = new Audio('dont_like.wav');
-        music.muted = true;
-        music.play();
+        music1.muted = true;
+        music1.play();
     }
     else if (sound == 'harrison') {
-        const music = new Audio('bad_feeling.wav');
-        music.muted = true;
-        music.play();
+        
+        music2.play();
     }
     else {
-        const music = new Audio('blaster.wav');
-        music.muted = true;
-        music.play();
+        music3.play();
     }
 
 }
 
 
-buttonElement1.addEventListener('click', sound_play("carrie"))
-buttonElement2.addEventListener('click', sound_play("harrison"))
-buttonElement3.addEventListener('click', sound_play("blaster"))
