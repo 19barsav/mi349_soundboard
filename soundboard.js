20f8d0328d23174a510725e07d7e014fded50499
19barsav <barne329@msu.edu>
2022-06-21 20:30:15 -0400

    maybe like this

--- soundboard.js
+++ soundboard.js
@@ -3,16 +3,17 @@ var buttonElement2 = document.getElementById('harrison')
 var buttonElement3 = document.getElementById('blaster')
 
 
-const music1 = new Audio('dont_like.wav');
+
 const music2 = new Audio('bad_feeling.wav');
 const music3 = new Audio('blaster.wav');
-        
-        
+
+var audio = document.createElement("AUDIO")
+document.body.appendChild(audio);
+audio.src = "dont_like.wav"
 
 var sound_play = function (sound) {
     if (sound == 'carrie') {
-        music1.muted = true;
-        music1.play();
+        audio.play();
     }
     else if (sound == 'harrison') {
         
@@ -25,6 +26,3 @@ var sound_play = function (sound) {
 }
 
 
-buttonElement1.addEventListener('click', sound_play("carrie"))
-buttonElement2.addEventListener('click', sound_play("harrison"))
-buttonElement3.addEventListener('click', sound_play("blaster"))
